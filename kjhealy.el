a7a41717ad49b732c8a1a56b1b9a69e24dde2807
Kieran Healy <kjhealy@gmail.com>
2009-01-07 12:23:54 -0700

    trying new comment key

--- kjhealy.el
+++ kjhealy.el
@@ -269,6 +269,8 @@
 ;; Ergonomic Keybindings for Emacs: http://xahlee.org/emacs/ergonomic_emacs_keybinding.html
 (load-file "~/elisp/vendor/ergonomic_keybinding_qwerty.el")
 
+;; Tweak to ergo keybindings for commenting regions of text
+(global-set-key (kbd "M-'") 'comment-or-uncomment-region-or-line)
 
 ;; Base dir
 (cd "~/")
