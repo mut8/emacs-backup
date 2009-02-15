43474c84b2541146b747cb20d831de6a8fc1ebc0
Kieran Healy <kjhealy@gmail.com>
2009-02-15 11:40:58 -0700

    adding yasnippet

--- kjhealy.el
+++ kjhealy.el
@@ -266,8 +266,10 @@
 ;; Put the menu bar back
 (menu-bar-mode 1)
 
-;; Ergonomic Keybindings for Emacs: http://xahlee.org/emacs/ergonomic_emacs_keybinding.html
-;;(load-file "~/elisp/vendor/ergonomic_keybinding_qwerty.el")
+;; yasnippet
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/elisp/vendor/yasnippet/snippets")
 
 ;; Tweak to ergo keybindings for commenting regions of text
 (global-set-key (kbd "M-'") 'comment-or-uncomment-region)
