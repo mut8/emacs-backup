e0b1c3293f6c57cf0b78e17ee8883552ecb34dba
Kieran Healy <kjhealy@gmail.com>
2010-06-24 10:52:12 -0400

    stupid missing paren

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;; This is the first thing to get loaded.
 ;;
 
-setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 
 ;; org-mode windmove compatibility
 (setq org-replace-disputed-keys t)
