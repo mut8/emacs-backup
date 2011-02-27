cccb82d43dd1900816dec880c31636c6c6743ba1
Kieran Healy <kjhealy@gmail.com>
2011-02-26 22:01:24 -0500

    test for PragmataPro (or in principle any font) and set it immediately as the default font if it's found. This helps prevent a lot of irritating flicker and resizing when emacs starts up.

--- init.el
+++ init.el
@@ -20,13 +20,27 @@
 ;; Package Locations
 ;; Location of various local packages (in .emacs.d/vendor or .emacs.d/src)
 ;;  because I don't want to keep them in =/Applications/Emacs.app/= or in
-;;  =/usr/share/local/=. 
+;;  =/usr/share/local/=.
 
 (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-        (let* ((my-lisp-dir "~/.emacs.d/")
-              (default-directory my-lisp-dir))
-           (setq load-path (cons my-lisp-dir load-path))
-           (normal-top-level-add-subdirs-to-load-path)))
+    (let* ((my-lisp-dir "~/.emacs.d/")
+           (default-directory my-lisp-dir))
+      (setq load-path (cons my-lisp-dir load-path))
+      (normal-top-level-add-subdirs-to-load-path)))
+
+;; Font-face setup. If PragmataPro exists, set it as the default font
+;; immediately, together with the background and foreground colors
+;; we'll use later when the color theme is set. I do this here to
+;; prevent some of the irritating flickering and resizing that
+;; otherwise goes on during startup. If you don't have PragmataPro
+;; installed, nothing will happen here. Alternatively, you can replace
+;; "PragmataPro" with the name of your font of choice.
+(defun font-existsp (font)
+  (if (null (x-list-fonts font))
+      nil t))
+
+(if (font-existsp "PragmataPro")
+    (set-face-attribute 'default nil :background "#1E1E1E" :foreground "#CACACA" :height 141 :font "PragmataPro"))
 
 ;; Load up Org Mode and Babel
 (require 'org-install)
